513c8653e3c04af07fad8a9cf828c74b72af4267
jeromericks <jeromericks@gmail.com>
2015-11-04 12:10:34 -0600

    Added an if statement to check the guess value and only count if the number is valid

--- highlow.php
+++ highlow.php
@@ -6,9 +6,13 @@ fwrite(STDOUT, 'Guess? ' . PHP_EOL);
 
 do {
 	$guess = trim(fgets(STDIN));
-	$counter++;
+	if($guess >= 1 && $guess <= 100) {
+		$counter++;
+	}
 	if(!is_numeric($guess)) {
-		fwrite(STDOUT, "Try again" . PHP_EOL . 'Guess? ' . PHP_EOL);
+		fwrite(STDOUT, "Must guess a number between 1 and 100" . PHP_EOL . 'Guess? ' . PHP_EOL);
+	} else if($guess <= 0 || $guess >= 101) {
+		fwrite(STDOUT, "Must guess a number between 1 and 100" . PHP_EOL . 'Guess? ' . PHP_EOL);
 	} else if($guess == $random) {
 		fwrite(STDOUT, 'GOOD GUESS!' . PHP_EOL . "Number of guesses: {$counter}" . PHP_EOL);
 	} else if($guess < $random) {
